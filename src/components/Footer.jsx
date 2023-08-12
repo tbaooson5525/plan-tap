a86bedf6e9f5629e3d1ea4adabd01016c34be5b2
Bao Son <tbaoson5525@gmail.com>
2023-08-13 01:58:32 +0700

    init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div>
-            <p>Ráng tập đàng quàng chăm chỉ cho đẹp cho đỡ chằm cảm nha <3 iu iu</p>
+            <p>Ráng tập đàng quàng chăm chỉ cho đẹp cho đỡ chằm cảm nha,iu iu</p>
         </div>
     )
 }
